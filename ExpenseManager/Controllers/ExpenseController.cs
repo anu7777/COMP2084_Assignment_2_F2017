512c7b286dd138edfa64b93a7d09b6da531fafe9
Satwinder Dhillon <satwinderdhillon429@gmail.com>
2017-12-15 13:24:37 +0530

    Add project files.

---
+++ ExpenseManager/Controllers/ExpenseController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ExpenseManager;
+
+namespace ExpenseManager.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        private ExpenseManagerDBEntities db = new ExpenseManagerDBEntities();
+
+        // GET: Expense
+        public ActionResult Index()
+        {
+            var expenses = db.Expenses.Include(e => e.Friend);
+            return View(expenses.ToList());
+        }
+
+        // GET: Expense/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Expens expens = db.Expenses.Find(id);
+            if (expens == null)
+            {
+                return HttpNotFound();
+            }
+            return View(expens);
+        }
+
+        // GET: Expense/Create
+        public ActionResult Create()
+        {
+            ViewBag.Friend_Id = new SelectList(db.Friends, "Id", "Name");
+            return View();
+        }
+
+        // POST: Expense/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Amount,SpentOn,Info,Friend_Id")] Expens expens)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Expenses.Add(expens);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Friend_Id = new SelectList(db.Friends, "Id", "Name", expens.Friend_Id);
+            return View(expens);
+        }
+
+        // GET: Expense/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Expens expens = db.Expenses.Find(id);
+            if (expens == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Friend_Id = new SelectList(db.Friends, "Id", "Name", expens.Friend_Id);
+            return View(expens);
+        }
+
+        // POST: Expense/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Amount,SpentOn,Info,Friend_Id")] Expens expens)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(expens).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Friend_Id = new SelectList(db.Friends, "Id", "Name", expens.Friend_Id);
+            return View(expens);
+        }
+
+        // GET: Expense/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Expens expens = db.Expenses.Find(id);
+            if (expens == null)
+            {
+                return HttpNotFound();
+            }
+            return View(expens);
+        }
+
+        // POST: Expense/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Expens expens = db.Expenses.Find(id);
+            db.Expenses.Remove(expens);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
